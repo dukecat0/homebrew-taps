a71ee7c0a24726d8be58acada0cf529a0728f195
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2021-10-30 14:16:34 +0800

    Add volume.rb

---
+++ volume.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Volume < Formula
+    homepage "https://github.com/meowmeowmeowcat/volume"
+    url "https://github.com/meowmeowmeowcat/volume.git",
+        :using => :git,
+        :tag => "v0.0.1"
+    head "https://github.com/meowmeowmeowcat/volume.git"
+
+    def install
+        bin.install "volume"
+    end
+
+    test do
+        system "#{bin}/volume"
+    end
+end
