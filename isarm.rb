a43b5d6f1e056493d4a674ff88dfafec24e93857
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2021-10-30 14:05:55 +0800

    Update isarm.rb

--- isarm.rb
+++ isarm.rb
@@ -3,8 +3,7 @@ require "formula"
 class Isarm < Formula
     homepage "https://github.com/meowmeowmeowcat/isarm"
     url "https://github.com/meowmeowmeowcat/isarm.git"
-    head "https://github.com/meowmeowmeowcat/isarm.git",
-        branch: "main"
+    head "https://github.com/meowmeowmeowcat/isarm.git"
 
     def install
         bin.install "isarm"
