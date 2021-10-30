0f1d374023fb59a0284bf000b9d3a199f57186e0
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2021-10-30 14:27:33 +0800

    Update volume.rb

--- volume.rb
+++ volume.rb
@@ -2,14 +2,13 @@ require "formula"
 
 class Volume < Formula
     homepage "https://github.com/meowmeowmeowcat/volume"
-    url "https://github.com/meowmeowmeowcat/volume/releases/download/v0.0.1/volume",
-    head "https://github.com/meowmeowmeowcat/volume.git"
+    url "https://github.com/meowmeowmeowcat/volume/releases/download/v0.0.1/volume"
 
     def install
         bin.install "volume"
     end
 
     test do
-        system "#{bin}/volume"
+        system "#{bin}/volume -v"
     end
 end
