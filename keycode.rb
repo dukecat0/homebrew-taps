03eb80d168879207a0ca89e130198d31d9d67ad1
meowmeowcat <meowmeowcat1211@gmail.com>
2021-11-22 00:14:14 +0800

    Update keycode.rb

--- keycode.rb
+++ keycode.rb
@@ -7,6 +7,7 @@ class Keycode < Formula
     sha256 "ed3839f44195fb92e880ad48544a042248d4c07fc6ef6d47c723924a42b0f7cb"
   else
     sha256 "98b0aa5c93b670cdca25ac8e614a8c56fd8b09e4ef5f5eee4ba142a9a39b0d56"
+  end
   url "https://github.com/meowmeowmeowcat/keycode/releases/download/v0.0.1/keycode-#{arch}"
   
   def install
