190faf935c4383c24dd2f0c1dd2f5163ac6795ce
meowmeowcat <meowmeowcat1211@gmail.com>
2021-11-22 00:12:11 +0800

    Update keycode.rb

--- keycode.rb
+++ keycode.rb
@@ -3,10 +3,14 @@ require "formula"
 class Keycode < Formula
   homepage "https://github.com/meowmeowmeowcat/keycode"
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+  if Hardware::CPU.intel?
+    sha256 "ed3839f44195fb92e880ad48544a042248d4c07fc6ef6d47c723924a42b0f7cb"
+  else
+    sha256 "98b0aa5c93b670cdca25ac8e614a8c56fd8b09e4ef5f5eee4ba142a9a39b0d56"
   url "https://github.com/meowmeowmeowcat/keycode/releases/download/v0.0.1/keycode-#{arch}"
   
   def install
-    bin.install "keycode"
+    bin.install "keycode=#{arch}" => "keycode"
   end
   
   test do
